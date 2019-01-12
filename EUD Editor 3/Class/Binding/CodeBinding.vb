f0ea3e3af87636580f44cc6916248baf1d8c3d2f
이정훈 <sksljh2091@naver.com>
2019-01-12 15:00:47 +0900

    리스트수술

--- EUD Editor 3/Class/Binding/CodeBinding.vb
+++ EUD Editor 3/Class/Binding/CodeBinding.vb
@@ -1,4 +1,15 @@
-﻿Public Class CodeBinding
+﻿Imports System.ComponentModel
+
+Public Class CodeBinding
     Implements INotifyPropertyChanged
 
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+
+
+
+
+    Private Sub NotifyPropertyChanged(ByVal info As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(info))
+    End Sub
 End Class
