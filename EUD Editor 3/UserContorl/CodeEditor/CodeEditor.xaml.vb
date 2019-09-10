dfa898604cff747e8c25616c4dbd682ef87221e7
이정훈 <sksljh2091@naver.com>
2019-09-10 20:22:41 +0900

    0.7.0.5

--- EUD Editor 3/UserContorl/CodeEditor/CodeEditor.xaml.vb
+++ EUD Editor 3/UserContorl/CodeEditor/CodeEditor.xaml.vb
@@ -1,4 +1,6 @@
-﻿Public Class CodeEditor
+﻿Imports System.Windows.Threading
+
+Public Class CodeEditor
     Public Property Text As String
         Get
             Return TextEditor.Text
@@ -29,11 +31,22 @@
         PopupToolTip.Close()
     End Sub
 
+    Private sMessageQueue As MaterialDesignThemes.Wpf.SnackbarMessageQueue
+
+    Private timer As DispatcherTimer
     Private Sub UserControl_Loaded(sender As Object, e As RoutedEventArgs)
         PopupToolTip = New PopupToolTip
         PopupToolTip.Show()
 
         PopupToolTip.Visibility = Visibility.Hidden
+
+        sMessageQueue = New MaterialDesignThemes.Wpf.SnackbarMessageQueue
+        ErrorSnackbar.MessageQueue = sMessageQueue
+
+
+        timer = New DispatcherTimer()
+        timer.Interval = TimeSpan.FromMilliseconds(100)
+        AddHandler timer.Tick, AddressOf timer_Tick
     End Sub
 
     Private Sub UserControl_LostFocus(sender As Object, e As RoutedEventArgs)
@@ -44,11 +57,50 @@
         RaiseEvent TextChange(Me, Nothing)
     End Sub
 
+
+    Private time As Integer
+    Private Sub timer_Tick(sender As Object, e As EventArgs)
+        time += 1
+        If time > 10 Then
+            ErrorSnackbar.IsActive = False
+            timer.Stop()
+        End If
+    End Sub
+
+
+
+
     Private Sub TextEditor_MouseWheel(sender As Object, e As MouseWheelEventArgs)
-        If e.Delta > 0 Then
-            TextEditor.FontSize += 1
-        ElseIf e.Delta < 0 Then
-            TextEditor.FontSize -= 1
+        If isctrl Then
+            If e.Delta > 0 Then
+                TextEditor.FontSize += 1
+            ElseIf e.Delta < 0 Then
+                If TextEditor.FontSize > 1 Then
+                    TextEditor.FontSize -= 1
+                End If
+            End If
+            'sMessageQueue.Enqueue("FontSize : " & TextEditor.FontSize & "px (기본 14px)")
+
+            ErrorSnackbar.IsActive = True
+            SnackbarContent.Content = "FontSize : " & TextEditor.FontSize & "px (기본 16px)"
+            time = 0
+            timer.Start()
+        End If
+    End Sub
+
+
+
+
+    Private isctrl As Boolean
+    Private Sub UserControl_PreviewKeyDown(sender As Object, e As KeyEventArgs)
+        If e.Key = Key.LeftCtrl Then
+            isctrl = True
+        End If
+    End Sub
+
+    Private Sub UserControl_PreviewKeyUp(sender As Object, e As KeyEventArgs)
+        If e.Key = Key.LeftCtrl Then
+            isctrl = False
         End If
     End Sub
 End Class
