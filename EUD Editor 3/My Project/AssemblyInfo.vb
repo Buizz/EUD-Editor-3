10c25bfdc5c758ad3f294ddb991536898acc7676
이정훈 <sksljh2091@naver.com>
2019-09-12 04:56:17 +0900

    0.7.0.6

--- EUD Editor 3/My Project/AssemblyInfo.vb
+++ EUD Editor 3/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' 지정되도록 할 수 있습니다.
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.7.0.5")>
-<Assembly: AssemblyFileVersion("0.7.0.5")>
+<Assembly: AssemblyVersion("0.7.0.6")>
+<Assembly: AssemblyFileVersion("0.7.0.6")>
