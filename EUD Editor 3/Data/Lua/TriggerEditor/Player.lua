d29ed978a57b6097c7bf807dc976d483d2cdcbc9
이정훈 <sksljh2091@naver.com>
2021-07-31 00:46:17 +0900

    0.12.6.3

--- EUD Editor 3/Data/Lua/TriggerEditor/Player.lua
+++ EUD Editor 3/Data/Lua/TriggerEditor/Player.lua
@@ -21,7 +21,7 @@
 function SetPlayerID(Player, ID, Args) --플레이어/TrgPlayer,FormatString,Arguments/[Player]의 아이디를 [ID][Args]로 변경합니다.
 	--SetPNamef(cp, "[플레이어] {:n}", cp);
 	--SetPNamef(cp, "{:t} \x07레벨: \x04{} {:c}{:n}", title, level, cp, cp);
-
+	Player = ParsePlayer(Player)
 	if Args == "" then
 		echo("SetPNamef(" .. Player .. ", \"" .. ID .. "\")")
 	else
@@ -49,5 +49,6 @@ end
 ]================================]
 function PlayerID(Player, ID) --플레이어/TrgPlayer,TrgString/[Player]의 아이디가 [ID]인지 확인합니다.
 	--IsPName(player, name)
+	Player = ParsePlayer(Player)
 	echo("IsPName(" .. Player .. ", \"" .. ID .. "\")")
 end
