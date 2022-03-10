74e830e15f3444e649dd6accb18fa43aa4ed9290
armoha <kein0011@naver.com>
2022-03-10 19:48:18 +0900

    Optimize GetDeaths

--- EUD Editor 3/Data/Lua/TriggerEditor/GetDeaths.lua
+++ EUD Editor 3/Data/Lua/TriggerEditor/GetDeaths.lua
@@ -20,10 +20,10 @@
 @param.Player.TrgPlayer
 대상 플레이어입니다.
 ]================================]
-function GetDeaths(Unit,Player) --일반/TrgUnit,TrgPlayer/[Player]의 [Unit] 데스값을 반환합니다.
-	Unit = ParseUnit(Unit)
+function GetDeaths(Unit, Player) -- 일반/TrgUnit,TrgPlayer/[Player]의 [Unit] 데스값을 반환합니다.
+    Unit = ParseUnit(Unit)
     Player = ParsePlayer(Player)
 
-	str = string.format("dwread_epd(EPD(0x58A364 + (%s * 12 + %s) * 4))",Unit , Player)
-	echo(str)
+    str = string.format("dwread_epd(%s * 12 + %s)", Unit, Player)
+    echo(str)
 end
