7eeb252c33972910867a931898b504639f3ae50f
이정훈 <sksljh2091@naver.com>
2025-05-31 20:29:57 +0900

    SCA 크기 조정기능 추가

---
+++ EUD Editor 3/Class/Binding/ValidationRule/NotTextAndOver3ValidationRule.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Globalization
+
+Public Class NotTextAndOver3ValidationRule
+    Inherits ValidationRule
+
+    Public Overrides Function Validate(value As Object, cultureInfo As CultureInfo) As ValidationResult
+        '일단 숫자인지 아닌지 판단.
+        Try
+            Dim number As Long = value
+
+            If number < 3 Then
+                Return New ValidationResult(False, "3보다 큰 숫자를 입력하세요.")
+            End If
+
+            Return ValidationResult.ValidResult
+        Catch ex As Exception
+            Return New ValidationResult(False, "숫자를 입력하세요")
+            'Return New ValidationResult(False, value.ToString)
+        End Try
+
+
+    End Function
+End Class
